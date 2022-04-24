4a14641d06ffb5b3d3fb7356abb78e894e18793b
Ariel Piñeiro <arielsrv@gmail.com>
2022-04-24 18:40:51 -0300

    sugar

--- Core/Shared/Http/Parsers/StringExtensions.cs
+++ Core/Shared/Http/Parsers/StringExtensions.cs
@@ -6,27 +6,26 @@ public static class StringExtensions
 {
     public static IEnumerable<long> ToEnumerable(this string values)
     {
-        const int maxConcurrent = 10;
-        IEnumerable<long> result = values
+        return values
             .Split(',')
             .Where(value => !string.IsNullOrWhiteSpace(value))
-            .Select((value, index) =>
-            {
-                if (!long.TryParse(value, out long number))
-                {
-                    throw new ApiBadRequestException("Invalid format for ids. e.g: ...&ids=1,2,3");
-                }
-
-                if (index > maxConcurrent - 1)
-                {
-                    throw new ApiBadRequestException($"Limit for multi-get is {maxConcurrent}");
-                }
-
-                return number;
-            })
+            .Select((value, index) => TryToGetNumber(value, index, 10))
             .Distinct()
             .ToList();
+    }
 
-        return result;
+    private static long TryToGetNumber(string value, int index, int maxConcurrent)
+    {
+        if (!long.TryParse(value, out long number))
+        {
+            throw new ApiBadRequestException("Invalid format for ids. e.g: ...&ids=1,2,3");
+        }
+
+        if (index > maxConcurrent - 1)
+        {
+            throw new ApiBadRequestException($"Limit for multi-get is {maxConcurrent}");
+        }
+
+        return number;
     }
 }
