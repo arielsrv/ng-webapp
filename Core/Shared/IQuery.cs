a5e3b048a84ab206444035aabd1a68144a7ab444
Ariel Piñeiro <arielsrv@gmail.com>
2022-04-26 08:30:44 -0300

    missing commit

--- Core/Shared/IQuery.cs
+++ Core/Shared/IQuery.cs
@@ -5,7 +5,6 @@ namespace Core.Shared;
 public interface IQuery<in TInput, TOutput>
 {
     IObservable<UserDto?> GetById(TInput id);
-    IObservable<IEnumerable<MultiGetDto<UserDto>>> GetById(IEnumerable<long> id);
     IObservable<IEnumerable<TOutput>> GetAll();
     IObservable<IEnumerable<MultiGetDto<TOutput>>> GetById(IEnumerable<long> elements);
 }
